6825e2e64d29acadd2392b671a09b8bdac26cf37
pauljeffreys1974 <35877133+pauljeffreys1974@users.noreply.github.com>
2025-07-01 12:30:33 -0400

    adding alan ireby site
    part 5

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+//function divDisplay(divToShow) {
+//	document.getElementById('homeDiv').style.visibility = "hidden";
+//	document.getElementById('homeDiv').style.display = "none";
+//	document.getElementById('aboutDiv').style.visibility = "hidden";
+//	document.getElementById('aboutDiv').style.display = "none";
+//	document.getElementById('jewelleryDiv').style.visibility = "hidden";
+//	document.getElementById('jewelleryDiv').style.display = "none";
+//	document.getElementById('crossstitchDiv').style.visibility = "hidden";
+//	document.getElementById('crossstitchDiv').style.display = "none";
+//	document.getElementById('galleryDiv').style.visibility = "hidden";
+//	document.getElementById('galleryDiv').style.display = "none";
+//
+//	document.getElementById(divToShow).style.visibility = "visible";
+//	document.getElementById(divToShow).style.display = "block";
+//}
